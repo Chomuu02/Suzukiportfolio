b7de06d2c4e367a9b9edf4653b6ff14ae2fb24a0
Suzuki <ty_suke_0u0@outlook.jp>
2024-04-09 06:20:02 +0900

    保存

---
+++ docs/JS/mouse.js
@@ -0,0 +1,7 @@
+
+//type="text/javascript"
+if(1){//左の1を0にすると下３行の設定を無視してデフォルトに戻せる（かんたん切り替え機能）
+    var colour='random';//左のコーテーションのペア内を #f0f や red などに変更し色の指定可能
+    var sparkles=50;//きらきらの数
+    var lower_sparkles=10;//きらきらが降り始める位置
+}//end if commentout
